c4df073c926c28a4308a29b245f85bd0c4668a4b
nipunraj <nipunraj99@gmail.com>
2023-11-26 15:10:39 +0530

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is about page.</p>
+      <Link href="/">Home</Link>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>This is contact page.</p>
+      <Link href="/">Home</Link>
+    </div>
+  );
+}
+
+export default Contact;

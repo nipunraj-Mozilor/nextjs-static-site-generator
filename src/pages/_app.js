9b416aa285adb5bb08052aa1e02d663ca3ce8aa4
nipunraj <nipunraj99@gmail.com>
2023-11-26 15:24:30 +0530

    cookieyes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,5 @@ import "@/styles/globals.css";
 import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return;
-  (
-    <Script
-      src="https://cdn-cookieyes.com/client_data/bdb26a65eba174901d8259a7/script.js"
-      strategy="beforeInteractive"
-    ></Script>
-  ),
-    (<Component {...pageProps} />);
+  return <Component {...pageProps} />;
 }

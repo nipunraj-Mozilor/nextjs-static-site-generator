90903c9ad62d3ff21a5e435ee39225b20163518d
nipunraj <nipunraj99@gmail.com>
2023-11-26 15:29:16 +0530

    cookeyes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,5 +2,13 @@ import "@/styles/globals.css";
 import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script
+        src="https://cdn-cookieyes.com/client_data/bdb26a65eba174901d8259a7/script.js"
+        strategy="beforeInteractive"
+      />
+    </>
+  );
 }
